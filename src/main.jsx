87a16a68048d9162e68648451bc4c71d591a556a
umartils <umartilmisani48@gmail.com>
2026-04-08 17:58:54 +0700

    Update assets

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Navbar from './components/Navbar.jsx'
-import Footer from './components/Footer.jsx'
-import PreLoader from './components/PreLoader.jsx'
+/* eslint-disable react/react-in-jsx-scope */
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import PreLoader from './components/PreLoader';
 
-import 'remixicon/fonts/remixicon.css'
+import 'remixicon/fonts/remixicon.css';
 import 'animate.css';
 
 import AOS from 'aos';
@@ -16,11 +17,11 @@ AOS.init();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div className='container mx-auto px-4'>
+    <div className="container mx-auto px-4">
       <PreLoader />
       <Navbar />
       <App />
       <Footer />
     </div>
-  </StrictMode>,
-)
+  </StrictMode>
+);
